25f2c367c88d8e8497c21e5420b43b505ea93b01
sadiq <sadiq810@gmail.com>
2023-10-16 16:51:21 +0500

    initial commit

---
+++ src/Application.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package    jodit
+ *
+ * @author     Valeriy Chupurnov <chupurnov@gmail.com>
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ * @link       https://xdsoft.net/jodit/
+ */
+
+namespace Jodit;
+
+use Jodit\components\BaseApplication;
+use Jodit\actions\Folder;
+use Jodit\actions\Files;
+use Jodit\actions\File;
+use Jodit\actions\Image;
+use Jodit\actions\Permissions;
+
+/**
+ * Class Application
+ * @package Jodit
+ */
+abstract class Application extends BaseApplication {
+	use Files;
+	use Folder;
+	use File;
+	use Image;
+	use Permissions;
+}
